a0c6b920b53bdc5abd09cc9178bfa6f3d2884833
xisux <87016182+xisux@users.noreply.github.com>
2022-04-27 12:21:10 +0200

    Added a config JSON

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from intpl import tools
 import json
 
+config_json = open('config.json')
+config = json.load(config_json)
+
+login = config['login']
+password = config['password']
+recovery = config['recovery']
+
 
 for x in range(5):
-    tools.create_account("isu", "passX18chars", "recovery@gmail.com")
+    tools.create_account(login, password, recovery)
